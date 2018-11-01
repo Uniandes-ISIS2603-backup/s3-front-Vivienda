fe5b7b4474272f24dd634febbde41bd0816cfb99
DESKTOP-ARSUDK0\Daniel <d.giraldo12@uniandes.edu.co>
2018-11-01 10:43:14 -0500

    .

--- src/app/vivienda/vivienda.service.ts
+++ src/app/vivienda/vivienda.service.ts
@@ -2,25 +2,25 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Vivienda} from "./vivienda";
-import { ViviendaDetail } from './vivienda-detail';
+import {ViviendaDetail} from './vivienda-detail';
 
-import { environment } from '../../environments/environment';
+import {environment} from '../../environments/environment';
+
 const API_URL = environment.apiURL;
 const viviendas = '/viviendas';
+
 @Injectable()
 export class ViviendaService {
 
-  constructor(private http: HttpClient) 
-  {
+  constructor(private http: HttpClient) {
   }
 
   getViviendas(): Observable<Vivienda[]> {
 
     return this.http.get<Vivienda[]>(API_URL + viviendas);
   }
-  
-  getViviendaDetail(viviendaId): Observable<ViviendaDetail>
-  {
-      return this.http.get<ViviendaDetail>(API_URL + viviendas + "/" + viviendaId);
+
+  getViviendaDetail(viviendaId): Observable<ViviendaDetail> {
+    return this.http.get<ViviendaDetail>(API_URL + viviendas + "/" + viviendaId);
   }
 }
