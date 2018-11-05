5d3f00081e221c958f42ee86f771e7d798d20d63
estudiante <pa.molina@uniandes.edu.co>
2018-11-05 12:53:18 -0500

    Despliegue lista universidades

--- src/app/universidad/universidad.service.spec.ts
+++ src/app/universidad/universidad.service.spec.ts
@@ -1,12 +1,30 @@
-import { TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppRoutingModule } from '../../routing-module/app-routing.module';
+import { APP_BASE_HREF } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
-import { UniversidadService } from './universidad.service';
+import { AppModule } from '../../app.module';
 
-describe('UniversidadService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+import { UniversidadListComponent } from './universidad-list.component';
+import {UniversidadService} from '../universidad.service';
+import { Universidad } from '../universidad';
 
-  it('should be created', () => {
-    const service: UniversidadService = TestBed.get(UniversidadService);
-    expect(service).toBeTruthy();
-  });
-});
+describe('UniversidadListComponent', () => {
+    let component: UniversidadListComponent;
+    let fixture: ComponentFixture<UniversidadListComponent>;
+    const universidades: Universidad[] = require('../../../assets/universidad.json');
+    
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [ AppRoutingModule, HttpClientModule, AppModule ],
+            declarations: [ ],
+            providers: [{provide: APP_BASE_HREF, useValue: ''}, UniversidadService ]
+        })
+        .compileComponents();
+    }));
+    
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UniversidadListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
--- src/app/universidad/universidad.service.ts
+++ src/app/universidad/universidad.service.ts
@@ -3,16 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Universidad} from './universidad';
 
-import {environment} from '../../environments/environment';
-
-const API_URL = environment.apiURL;
-const universidades = '/universidades';
+const API_URL = 'http://localhost:8080/s3_vivienda-api/api/';
+const universidades = 'assets/universidades.json';
 
 @Injectable()
 export class UniversidadService {
    
    constructor(private http: HttpClient) { }
    getUniversidades() : Observable<Universidad[]> {
-   return this.http.get<Universidad[]>(API_URL + universidades);
+   return this.http.get<Universidad[]>(universidades);
    }
 }
