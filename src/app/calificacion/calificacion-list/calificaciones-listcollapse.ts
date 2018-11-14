cd3a428a7970535557f4cb7fb6f24018e08da3e3
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2018-11-13 23:01:11 -0500

    Lista de Calificaciones

---
+++ src/app/calificacion/calificacion-list/calificaciones-listcollapse.ts
@@ -0,0 +1,54 @@
+import {OnInit, Input} from '@angular/core';
+import {Calificacion} from '../calificacion';
+
+export class CalificacionListCollapseComponent implements OnInit {
+
+    /**
+     * List of calificacions to show
+     */
+    @Input() calificaciones: Calificacion[];
+    
+    calificacionesFiltradas: Calificacion[];
+    
+    fPuntajeMin:  number = 0;
+    fPuntajeMax:  number = 5;
+    
+    puntSort: string = "";
+    
+    /**
+     * Boolean attribute modeling the collapse button
+     */
+    public isCollapsed = true;
+    
+    
+    compPuntaje(a: Calificacion, b: Calificacion):number{
+        if (a.puntaje == b.puntaje)
+            return 0;
+        else
+            return (a.puntaje < b.puntaje)? -1:1;
+    }
+    
+    puntajeSort(){
+        this.calificacionesFiltradas = (this.puntSort != "") ? this.calificacionesFiltradas.reverse():
+                                        this.calificacionesFiltradas.sort(this.compPuntaje);
+        this.puntSort = (this.puntSort == "(asc)")? "(des)":"(asc)";
+    }
+    
+    verificarMin(): boolean{
+        if (this.fPuntajeMax < this.fPuntajeMin)
+             this.fPuntajeMin = this.fPuntajeMax;
+        return true;
+    }
+    
+    verificarMax(): boolean{
+        if (this.fPuntajeMax < this.fPuntajeMin)
+            this.fPuntajeMin = this.fPuntajeMax - 0.1;
+        return true;
+    }
+    /**
+     * Initializes the component
+     */
+    ngOnInit() {
+        this.calificacionesFiltradas = this.calificaciones;
+    }
+}
--- src/app/calificacion/calificacion-list/calificacionvivienda-listcollapse.component.ts
+++ src/app/calificacion/calificacion-list/calificacionvivienda-listcollapse.component.ts
@@ -1,30 +1,75 @@
 import {Component, OnInit, Input} from "@angular/core";
 import { Calificacion } from '../calificacion';
+import {CalificacionListCollapseComponent} from './calificaciones-listcollapse';
 
 @Component({
   selector: 'calificacion-vivienda-listcollapse',
-  templateUrl: './calificacion-listcollapse.component.html'
+  templateUrl: './calificacion-listcollapse.component.html',
+  styleUrls: ['./calificacion-listcollapse.component.css']
 })
-export class CalificacionViviendaListCollapseComponent implements OnInit{
-    
-    /**
-     * The list of reviews to show
-     */ 
-    @Input() calificaciones: Calificacion[];
-    
-    /**
-     * Boolean attribute modeling the collapse button
-     */
-    public isCollapsed = true;
+export class CalificacionViviendaListCollapseComponent extends CalificacionListCollapseComponent implements OnInit{
     
     /**
      * String referencing the type of table to show
      */
     listaDe: String = 'vivienda';
+    
+    fNombre: string;
+    fLogin: string;
+    
+    nombSort: string = "(asc)";
+    logiSort: string = "";
+
+
+    filtrar(): void{
+        this.calificacionesFiltradas = this.calificaciones.filter( cal =>{
+            return (!this.fNombre || cal.estudiante.nombre.toLowerCase().startsWith(this.fNombre.toLowerCase())) &&
+            (!this.fLogin || cal.estudiante.login.toLowerCase().startsWith(this.fLogin.toLowerCase())) &&
+                (! this.fPuntajeMin || cal.puntaje >= this.fPuntajeMin) &&
+                (! this.fPuntajeMax || cal.puntaje <= this.fPuntajeMax);
+        });
+    }
+    
+    compNombre (a: Calificacion, b: Calificacion):number{
+        if (a.estudiante.nombre == b.estudiante.nombre)
+            return 0;
+        else
+            return (a.estudiante.nombre < b.estudiante.nombre)? -1:1;
+    }
+    
+    nombreSort(){
+        this.calificacionesFiltradas = (this.nombSort != "") ? this.calificacionesFiltradas.reverse():
+                                        this.calificacionesFiltradas.sort(this.compNombre);
+        this.nombSort = (this.nombSort == "(asc)")? "(des)":"(asc)";
+        this.puntSort = "";
+        this.logiSort = "";
+    }
+    
+    compLogin (a: Calificacion, b: Calificacion):number{
+        if (a.estudiante.login == b.estudiante.login)
+            return 0;
+        else
+            return (a.estudiante.login < b.estudiante.login)? -1:1;
+    }
+    
+    loginSort(){
+        this.calificacionesFiltradas = (this.logiSort != "") ? this.calificacionesFiltradas.reverse():
+                                        this.calificacionesFiltradas.sort(this.compLogin);
+        this.logiSort = (this.logiSort == "(asc)")? "(des)":"(asc)";
+        this.puntSort = "";
+        this.nombSort = "";
+    }
+    
+    puntajeSort(){
+        super.puntajeSort();
+        this.nombSort = "";
+        this.logiSort = "";
+    }
 
     /**
      * Initializes the component
      */
     ngOnInit() {
+        this.calificacionesFiltradas = this.calificaciones;
     }
 }
